bc627d69e8337a3c4badd8b5797e7bb538bb470c
Matus Tomlein <matus@tomlein.org>
2013-06-03 14:34:08 +0200

    Improve HUD display

--- PCController/src/GrabberShow.java
+++ PCController/src/GrabberShow.java
@@ -1,8 +1,11 @@
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
 import com.googlecode.javacv.FrameGrabber;
 import com.googlecode.javacv.VideoInputFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
-import javax.swing.*;
 
 public class GrabberShow extends JPanel implements Runnable {
 	IplImage image;
@@ -11,13 +14,14 @@ public class GrabberShow extends JPanel implements Runnable {
 	int height;
 	
 	public GrabberShow(int w, int h) {
+		super();
 		width = w;
 		height = h;
 	}
 	
 	@Override
 	public void run() {
-		FrameGrabber grabber = new VideoInputFrameGrabber(2); // 1 for next camera
+		FrameGrabber grabber = new VideoInputFrameGrabber(1); // 1 for next camera
 		try {
 			grabber.start();
 			
@@ -33,10 +37,12 @@ public class GrabberShow extends JPanel implements Runnable {
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		g.drawImage(img.getBufferedImage(), 0, 0, width, height, null);
-}
-	
-	
+		super.paintComponent(g);
+		if (img != null) {
+			Graphics2D g2 = (Graphics2D) g;
+			g2.drawImage(img.getBufferedImage(), 0, 0, width, height, null);
+		}
+	}
 }
 
 
